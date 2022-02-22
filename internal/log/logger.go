783e8d59023672fe4fc1ede26ba14b6363483a79
Braden <braden@bradenn.com>
2022-02-21 19:37:56 -0800

    Initialized Go project with basic file structure, including a basic trackable logging system.

---
+++ internal/log/logger.go
@@ -0,0 +1,34 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+const (
+	name       = "WildLife"
+	colorGreen = "\u001B[1;32m"
+	colorReset = "\u001B[m"
+)
+
+// printLn prints a formatted string to stdout
+func printLn(line string) {
+	fmt.Printf("%s[%s]%s %s%s\n", colorGreen, name, colorReset, line, colorReset)
+}
+
+// Logf will attempt to format a log message or print plain text.
+func Logf(format string, args ...any) {
+	// If debug tracing is enabled, the file and line number of a log function call will prefix the log message.
+	if os.Getenv("trace") == "true" {
+		// Get function caller
+		_, file, ln, ok := runtime.Caller(1)
+		// Add the caller file and line number prefix
+		if ok {
+			fmt.Printf("%s:%d -> ", filepath.Base(file), ln)
+		}
+	}
+	// Print the log message
+	printLn(fmt.Sprintf(format, args...))
+}
