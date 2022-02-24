ae5a43d0cfb55a13cd1247826f004982f0dc854a
Braden <braden@bradenn.com>
2022-02-24 12:34:48 -0800

    Added initial upload functionality

---
+++ client/src/components/Upload.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {reactive} from 'vue';
+
+const data = reactive({
+  uploaded: {
+    size: 0
+  },
+});
+
+function uploadFile(event) {
+  data.uploaded.size = event.target.size
+  console.log(event)
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>Upload Images</h1>
+    <div>
+      Size:{{ data.uploaded }}
+      <br>
+      <input id="upload" accept="image/png,image/jpg" class="file-upload" name="File" type="file" @change="uploadFile">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
