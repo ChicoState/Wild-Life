57cda07c07438f0558f66bf658883156210138dd
Ryan Fong <rybeats@live.com>
2022-02-24 21:27:54 -0800

    Resolves ChicoState/Wild-Life#11; Has a few extra tweaks beyond what's asked

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"os"
 	"wildlife/internal/log"
 	"wildlife/internal/server"
+	"wildlife/internal/server/controller"
+	"wildlife/internal/server/model"
+
+	"github.com/joho/godotenv"
 )
 
 const VERSION = "0.0.1"
@@ -10,6 +15,13 @@ const VERSION = "0.0.1"
 func main() {
 	log.Logf("Started v%s", VERSION)
 	// Start the web server
+	godotenv.Load()
+
+	if os.Getenv("DB_ACTIVE") == "true" {
+		model.InitDB()
+		controller.InitController()
+	}
+
 	err := server.Start()
 	if err != nil {
 		return
