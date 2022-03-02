3269479f96a852510d03fb2d68abf27e16cd8260
Braden <braden@bradenn.com>
2022-03-02 15:12:18 -0800

    Migrated early front-end to match UI design. Added upload js class.

--- client/src/components/Upload.vue
+++ client/src/components/Upload.vue
@@ -1,30 +1,56 @@
 <script setup>
 import {reactive} from 'vue';
 
+import {Upload} from '../rest.js';
+
 const data = reactive({
   uploaded: {
-    size: 0
+    status: 0,
+    size: 0,
+    name: 0,
   },
+  response: {}
 });
 
+function uploadSucceeded(res) {
+  data.response = res
+}
+
+function uploadFailed(error) {
+  data.response = error
+}
+
 function uploadFile(event) {
-  data.uploaded.size = event.target.size
-  console.log(event)
+
+  let upload = new Upload()
+
+  upload.addFile(event.target.files[0])
+
+  upload.onsuccess = uploadSucceeded
+  upload.onfail = uploadFailed
+
+  upload.submit()
 }
 
 </script>
 
 <template>
   <div>
-    <h1>Upload Images</h1>
+    <h2>Upload Images</h2>
     <div>
-      Size:{{ data.uploaded }}
-      <br>
-      <input id="upload" accept="image/png,image/jpg" class="file-upload" name="File" type="file" @change="uploadFile">
+      <label class="custom-file-upload button">
+        <input id="upload" accept="image/png,image/jpg" class="button" type="file" @change="uploadFile">
+        <i class="fa fa-cloud-upload"></i> Select File
+      </label>
+      {{ data.response }}
     </div>
   </div>
 </template>
 
 <style scoped>
+input[type="file"] {
+  display: none;
+}
+
 
 </style>
